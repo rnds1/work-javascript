a663ab00f5a81eafbd6d0046739d2cc02ed80639
Rafael N Santos <42522990+rnds1@users.noreply.github.com>
2023-06-04 19:42:34 -0300

    Create Cmp.Array.numbers.js
    v1 low level

---
+++ Cmp.Array.numbers.js
@@ -0,0 +1,12 @@
+var f = (x,y)=>{if(x==y){return true}else{return false}};
+for(i=0;i<nms.length;i++){
+    for(j=0;j<nms.length;j++){
+        if(i !=j){
+          if(  nms[i] == nms[j]){
+            
+            return true;
+                  
+            }
+        }
+    }
+}
